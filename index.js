abb28f2074e44a1152ee2169fa9b42fc98ec9bbf
heydonhatcher <heyhatch@gmail.com>
2019-11-16 16:52:07 -0600

    ready to grade

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+const port = process.env.PORT || 4000;
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const { users } = require("./state");
 
-const { users } = require('./state')
+function getUser(userId) {
+  let targetUser;
+  for (i = 0; i < users.length; i++) {
+    if (users[i]._id === userId) {
+      targetUser = users[i];
+    }
+  }
+  return targetUser;
+}
 
-/* BEGIN - create routes here */
+app.get("/users", (req, res) => {
+  res.json(users);
+});
 
+app.get("/users/:userId", (req, res) => {
+  let targetId = Number(req.params.userId);
+  console.log(targetId);
+  let targetUser = getUser(targetId);
+  res.json(targetUser);
+});
 
-/* END - create routes here */
+app.post("/users", (req, res) => {
+  let newUser = {
+    _id: 6,
+    name: "Charlie",
+    occupation: "Bartender",
+    avatar: "https://www.fillmurray.com/200/200"
+  };
+  users.push(newUser);
+  res.json(users);
+});
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.put("/users/:userId", (req, res) => {
+  let targetId = Number(req.params.userId);
+  let targetUser = getUser(targetId);
+  targetUser.occupation = "Kung Fu Fighter";
+  res.json(users);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  let targetId = Number(req.params.userId);
+  let targetUser = getUser(targetId);
+  targetUser.isActive = false;
+  res.send("deleted");
+  console.log(users);
+});
+
+var counter = 7;
+app.post("/addusers", (req, res) => {
+  req.body._id = counter;
+  counter += 1;
+  console.log(req.body);
+  users.push(req.body);
+  res.json(req.body);
+  console.log(users);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
